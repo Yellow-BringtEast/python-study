6dac346a6455afa1d3c977c1554842c7f50e8fec
Yellow-BringtEast <714376214@qq.com>
2020-06-14 11:48:16 +0800

    uodate string.py

--- 数据类型/string.py
+++ 数据类型/string.py
@@ -22,7 +22,7 @@ print("""\
 Usage: thingy [OPTIONS]
      -h                        Display this usage message
      -H hostname               Hostname to connect to
-""")    # 字符串中的回车换行会自动包含到字符串中，如果不想包含，在行尾添加一个\即可
+""")  # 字符串中的回车换行会自动包含到字符串中，如果不想包含，在行尾添加一个\即可
 
 # 字符串可以用 + 进行连接（粘到一起），也可以用 * 进行重复
 print("3 * un + ium")
@@ -32,7 +32,7 @@ print('Py''thon')
 
 # 把很长的字符串拆开分别输入的时候尤其有用
 text = ('Put several strings within parentheses '
-      'to have them joined together.')
+        'to have them joined together.')
 print(text)
 
 # 只能对两个字面值这样操作，变量或表达式不行
@@ -49,7 +49,7 @@ print(string[0])
 print(string[-1])  # 索引为负数时，表示从右边开始数，由于-0 = 0，所以右边第一个为-1
 
 # 字符串还支持切片，索引可以得到单个字符，而切片可以获取子字符串
-print(string[0:2])  #  切片的开始总是包括在结果中的，而结束不被包括
+print(string[0:2])  # 切片的开始总是包括在结果中的，而结束不被包括
 
 # 切片的索引有默认值；省略开始索引时默认为0，省略结束索引时默认为到字符串的结束
 print(string[:2])
@@ -66,6 +66,8 @@ print(string[4::42])
 a = len(string)
 print(a)
 
+# 循环遍历
+str1 = "hello python"
 
-
-
+for char in str1:
+    print(char)
