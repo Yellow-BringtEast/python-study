b989679ef37c532c50b37471e2023d83d7f482d2
Yellow-BringtEast <714376214@qq.com>
2022-07-09 09:15:25 +0800

    更新天眼查爬虫

--- 爬虫/tianyan_crawler.py
+++ 爬虫/tianyan_crawler.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver import ActionChains
 from PIL import Image
 from lxml import etree
+from typing import TypeVar
 
 import time
 import random
@@ -13,10 +14,25 @@ import json
 
 # 无头模式
 chrome_option = Options()
-chrome_option.add_argument('--headless')
+chrome_option.headless = True
+chrome_option.add_argument(
+    "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/103.0.5060.114 Safari/537.36")
+# 解决无头模式滑块图片截图不全的问题
+chrome_option.add_argument("-window-size=1920,1080")
+chrome_option.add_argument("--start-maximized")
 
+driver = webdriver.Chrome(options=chrome_option)
+
+# 登录及搜索地址
 LOGIN_URL = 'https://www.tianyancha.com/'
 SEARCH_URL = 'https://www.tianyancha.com/search?key='
+
+# 账号信息
+ACCOUNT = ''
+PWD = ''
+
+Json = TypeVar('Json')
 
 
 class GetCookies:
@@ -24,17 +40,22 @@ class GetCookies:
     使用selenium自动登录天眼查，获取cookies
     """
 
-    def __init__(self, account: str, pwd: str) -> None:
+    def __init__(self, account: str = ACCOUNT, pwd: str = PWD, is_headless: bool = True) -> None:
         """
         初始化参数
         :param account: 天眼查账号
-        :param pwd:  天眼查密码
+        :param pwd: 天眼查密码
+        :param is_headless: 是否启用无头模式
         """
         self.account = account
         self.pwd = pwd
 
+        if (is_headless == False):
+            global driver
+            driver = webdriver.Chrome()
+
     @staticmethod
-    def compare_images(image_be, image_af, i: int, j: int, threshold: int = 50) -> bool:
+    def compare_images(image_be, image_af, i: int, j: int, threshold: int = 60) -> bool:
         """
         判断两张图片相同位置的像素之间是否存相同
         :param image_be: 图片一
@@ -55,14 +76,12 @@ class GetCookies:
         else:
             return False
 
-    def get_cookies(self):
+    def get_cookies(self) -> dict:
         """
         登录天眼查，获取cookies
         :return: cookies
         """
         # 打开页面
-
-        driver = webdriver.Chrome(options=chrome_option)
         driver.get(LOGIN_URL)
 
         driver.maximize_window()
@@ -111,13 +130,14 @@ class GetCookies:
         size = img.size
 
         # 得到图片坐标
-        top, bottom, left, right = location['y'], location['y'] + size['height'], location['x'], location['x'] \
-                                   + size['width']
+        top, bottom, left, right = location['y'], location['y'] + size['height'], location['x'], \
+                                   location['x'] + size['width']
 
         # 截取滑块验证码完整图片
         screen_shot = driver.get_screenshot_as_png()
         screen_shot = Image.open(BytesIO(screen_shot))
         img_be = screen_shot.crop((int(left), int(top), int(right), int(bottom)))
+        # img_be.save('./be.png')
 
         # 点击滑块，获取带缺口的验证码图片
         time.sleep(random.random() * 2.5)
@@ -129,10 +149,11 @@ class GetCookies:
         screen_shot = driver.get_screenshot_as_png()
         screen_shot = Image.open(BytesIO(screen_shot))
         img_af = screen_shot.crop((int(left), int(top), int(right), int(bottom)))
+        # img_af.save('./af.png')
 
         # 对比滑块图片像素差异，获取滑块滑动距离
         # 从滑块右侧开始逐一对比寻找缺口
-        left = 60
+        left = 57
         has_find = False
 
         for i in range(left, img_be.size[0]):
@@ -145,7 +166,7 @@ class GetCookies:
                     break
 
         # 滑块移动距离
-        move = left - 10
+        move = left - 9
 
         # 移动滑块，完成登录
         ActionChains(driver).move_by_offset(move, 0).perform()
@@ -159,9 +180,29 @@ class GetCookies:
         for cookie in cookies_list:
             cookies_dict[cookie['name']] = cookie['value']
 
-        # 关闭浏览器
-        driver.close()
+        with open('./cookies.txt', 'w') as f:
+            f.write(str(cookies_dict))
 
+        return cookies_dict
+
+    def get_cookies_dict(self) -> dict:
+        """
+        出现错误时进行重试，保证获取到的cookie有效
+        :return: cookies
+        """
+        while True:
+            try:
+                cookies_dict = self.get_cookies()
+                if 'tyc-user-info' in cookies_dict.keys():
+                    driver.close()
+                    print('登录成功！')
+                    break
+                else:
+                    driver.refresh()
+                    print('登录失败，正在重试！')
+            except Exception as e:
+                driver.refresh()
+                print(e)
         return cookies_dict
 
 
@@ -170,7 +211,7 @@ class GetCompanyInfo:
     根据企业名称，获取企业注册地址，注册资本等信息
     """
 
-    def __init__(self, company_name, cookies_dict):
+    def __init__(self, cookies_dict, company_name: str = None):
         """
         初始化参数
         :param company_name: 公司名称
@@ -189,7 +230,7 @@ class GetCompanyInfo:
                       '*/*;q=0.8,application/signed-exchange;v=b3;q=0.9 '
         }
 
-    def get_company_url(self):
+    def get_company_url(self) -> str:
         """
         根据企业名称获取企业天眼查地址
         :return: 企业天眼查地址
@@ -205,11 +246,18 @@ class GetCompanyInfo:
 
         return company_url
 
-    def get_company_info(self):
+    def get_company_info(self, company_url=None) -> Json:
+        """
+        爬取企业天眼查地址中的有关信息
+        :param company_url: 企业天眼查地址
+        :return: 企业有关工商信息json
+        """
         # 企业天眼查地址
-        company_url = self.get_company_url()
+        if company_url is None:
+            company_url = self.get_company_url()
+
         headers = self.headers
-        headers['path'] = f"/company/{company_url.split('/')[-1]}"
+        headers['path'] = f"/company/{company_url.split('/')[-1]}".encode('utf-8')
 
         re = requests.get(url=company_url, headers=headers, cookies=self.cookies)
         html = etree.HTML(re.text)
@@ -224,6 +272,9 @@ class GetCompanyInfo:
 
 
 if __name__ == '__main__':
-    cookies = GetCookies('your_account', 'password').get_cookies()
-    com_data = GetCompanyInfo('阿里巴巴', cookies).get_company_info()
+    # 自动登录获取cookies
+    cookies = GetCookies(ACCOUNT, PWD, is_headless=False).get_cookies_dict()
+
+    # 获取企业信息
+    com_data = GetCompanyInfo(cookies, '上海蔚来汽车').get_company_info()
     print(com_data)
